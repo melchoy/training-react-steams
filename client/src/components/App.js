0bd23a6cbb3207e9074f52a7c15c9522e08d29ae
Melissa Choy <mel@melissachoy.com>
2019-11-21 21:10:52 +1100

    scaffolding for stream components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter, Router, Link } from 'react-router-dom';
 
 const App = () => {
   return <div>My App</div>
---
+++ client/src/components/streams/StramEdit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function StramEdit() {
+  return (
+    <div>
+      StreamEdit
+    </div>
+  )
+}
