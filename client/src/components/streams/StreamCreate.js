b3613ea7d544406c43bd0403e591320daf4321f0
Melissa Choy <mel@melissachoy.com>
2019-11-23 21:10:04 +1100

    highlighting error fields

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -13,8 +13,9 @@ class StreamCreate extends React.Component {
   }
 
   renderInput = ({input, label, meta}) => {    
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
     return(
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         <input {...input} />
         {this.renderError(meta)}     
@@ -28,7 +29,7 @@ class StreamCreate extends React.Component {
 
   render() {
     return(
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
         <Field name="title" label="Enter Title" component={this.renderInput} />
         <Field name="description" label="Enter Description" component={this.renderInput} /> 
         <button className="ui button primary">Submit</button>
