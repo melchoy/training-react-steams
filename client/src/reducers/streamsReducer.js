f409185307c594acf03b02d232474738779da1a5
Melissa Choy <mel@melissachoy.com>
2019-11-25 19:16:14 +1100

    deleting properties with omit

--- client/src/reducers/streamsReducer.js
+++ client/src/reducers/streamsReducer.js
@@ -9,11 +9,13 @@ import {
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAM:
-      return { ...state, [action.payload.id]: action.payload }
+      return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
-      return { ...state, [action.payload.id]: action.payload }
+      return { ...state, [action.payload.id]: action.payload };
     case EDIT_STREAM:
-      return { ...state, [action.payload.id]: action.payload }
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return DELETE_STREAM.omit(state, action.payload);
     default:
       return state;
   }
